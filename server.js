44ae83ca831691498ffd14bbbe8940fa295ecd09
Nathan Dev <nathandev33@gmail.com>
2022-07-01 11:23:27 +0200

    .

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 const User = require("./models/user");
 const Day = require("./models/day");
-// app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // app.use(express.json());
 // app.use(express.bodyParser());
